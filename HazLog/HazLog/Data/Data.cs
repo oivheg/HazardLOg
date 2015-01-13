366b606964f2b9f1b19b71ca7b12406bcb4e9d1d
oivheg <oivheg@gmail.com>
2015-01-13 15:41:09 +0100

    made simple shapes for mind map,

--- HazLog/HazLog/Data/Data.cs
+++ HazLog/HazLog/Data/Data.cs
@@ -1,24 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HazLog.Data
 {
-    class Data
+    internal class Data
     {
-        String Name;
+        private String Name;
+
         public Data()
         {
             var Name = "DataName";
-
         }
 
         public String getName()
         {
             return Name;
         }
-
     }
 }
